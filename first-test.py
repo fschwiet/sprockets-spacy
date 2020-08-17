a1ca9182ec80fe918b1554f0b674f790e1e90771
Frank <fschwiet@gmail.com>
2020-08-17 17:41:16 -0300

    detecting more verb phrase interconnections

--- first-test.py
+++ first-test.py
@@ -13,13 +13,18 @@ VerbPhraseConnection = collections.namedtuple("VerbPhraseConnection", ["mark", "
 
 
 class VerbPhrase:
-    # A verb and its helper verbs, object complements not included.
+    # A verb and its helper verbs, object complements are not included.
 
-    def __init__(self, verbs):
+    def __init__(self, verbs, sentence):
         if not any(verbs):
             raise Exception("VerbPhrase given no verbs")
         self.verbs = verbs
         self.connections = []
+
+        self.headOfCopula = None
+        [*_, lastVerb] = verbs;
+        if lastVerb.head > 0 and lastVerb.deprel == "cop":
+            self.headOfCopula = sentence.words[lastVerb.head - 1]
 
     @property
     def verb_string(self):
@@ -41,20 +46,17 @@ class SprocketSentence:
         for mark in (w for w in sentence.words if w.deprel == "mark"):
             if mark.head == 0:
                 continue
-
             tail = sentence.words[mark.head - 1]
+
             if tail.head == 0:
                 continue
-
             head = sentence.words[tail.head - 1]
-            if head.head == 0:
-                continue
 
-            head_verb_phrases = list(vp for vp in self.verbPhrases if head in vp.verbs)
+            head_verb_phrases = list(vp for vp in self.verbPhrases if (head in vp.verbs) or head is vp.headOfCopula)
             if not any(head_verb_phrases):
                 continue
 
-            tail_verb_phrases = list(vp for vp in self.verbPhrases if tail in vp.verbs)
+            tail_verb_phrases = list(vp for vp in self.verbPhrases if (tail in vp.verbs) or tail is vp.headOfCopula)
             if not any(tail_verb_phrases):
                 continue
 
@@ -69,6 +71,15 @@ class SprocketSentence:
     def summarize_verb_phrases(self):
         return list(vp.verb_string for vp in self.verbPhrases)
 
+    def summarize_interconnections(self):
+        return list(self._summarize_interconnections())
+
+    def _summarize_interconnections(self):
+        for [mark, head, tail, head_verb_phrase, tail_verb_phrase] in self.connections:
+            yield "{0} -{1}- {2}".format(head_verb_phrase.verb_string, mark.text, tail_verb_phrase.verb_string)
+
+    def __repr__(self):
+        return "<SprocketSentence({0},{1})>".format(self.summarize_verb_phrases(), self.summarize_interconnections())
 
 def get_word_feature(w, feature):
     if not hasattr(w, "features"):
@@ -106,39 +117,51 @@ def _get_verb_phrases(sentence):
             should_accumulate_word = True
 
         if should_publish_existing and any(accumulator):
-            yield VerbPhrase(accumulator)
+            yield VerbPhrase(accumulator, sentence)
             accumulator = []
 
         if should_accumulate_word:
             accumulator.append(word)
 
     if any(accumulator):
-        yield VerbPhrase(accumulator)
+        yield VerbPhrase(accumulator, sentence)
 
 
 class TestVerbPhraseExtraction(unittest.TestCase):
     def test_can_find_single_clause(self):
         sprocket = self.run_stanza("Había estado corriendo durante treinta minutos.")
         self.assertEqual(sprocket.summarize_verb_phrases(), ["Había estado corriendo"])
+        self.assertEqual(sprocket.summarize_interconnections(), []);
 
     def test_can_find_multiple_clauses(self):
         sprocket = self.run_stanza("Me es desagradable que comas haciendo tanto ruido.")
         self.assertEqual(sprocket.summarize_verb_phrases(), ["es", "comas", "haciendo"])
+        self.assertEqual(sprocket.summarize_interconnections(), ["es -que- comas"]);
+
+    def test_can_find_multiple_clauses_2(self):
+        # Unsourced sentence - created from previous to have a longer copula verb phrase
+        sprocket = self.run_stanza("Me ha sido desagradable que comas haciendo tanto ruido.")
+        self.assertEqual(sprocket.summarize_verb_phrases(), ["ha sido", "comas", "haciendo"])
+        self.assertEqual(sprocket.summarize_interconnections(), ["ha sido -que- comas"]);
 
     def test_can_recognize_soler_auxillary_usage(self):
         sprocket = self.run_stanza("Si éste solía funcionar, piense que es lo que ha cambiado en su sistema desde el momento que ha dejado de funcionar.")
         self.assertEqual(sprocket.summarize_verb_phrases(), ["solía funcionar", "piense", "es", "ha cambiado", "ha dejado-de funcionar"])
+        self.assertEqual(sprocket.summarize_interconnections(), ["piense -Si- solía funcionar", "piense -que- es"]);
 
     def test_el_hecho_is_excluded(self):
         sprocket = self.run_stanza("Los resultados escolares también están influidos por el hecho de que la mayoría de los alumnos no pueden pedir a sus padres.")
         self.assertEqual(sprocket.summarize_verb_phrases(), ["están influidos", "pueden pedir"]);
+        self.assertEqual(sprocket.summarize_interconnections(), []);
 
     @staticmethod
     def run_stanza(text):
-        result = nlp(text)
-        [sentence] = result.sentences
+        stanza_result = nlp(text)
+        [sentence] = stanza_result.sentences
         tabulate_sentence(sentence)
-        return SprocketSentence(sentence)
+        sprocket = SprocketSentence(sentence)
+        print(sprocket.summarize_interconnections())
+        return sprocket
 
 
 if __name__ == '__main__':
